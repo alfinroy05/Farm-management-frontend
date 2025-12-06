1d6f50f1729086253abad04e930d250086883fc7
alfinroy05 <alfinroy2005@gmail.com>
2025-12-07 00:22:11 +0530

    dashboard created

---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <h1 className="text-center mt-5">Welcome to the Dashboard</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard
