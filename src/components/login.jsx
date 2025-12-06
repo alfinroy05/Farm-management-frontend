7447665d0160dfe7d747a34c359b7f1a9c9e5477
alfinroy05 <alfinroy2005@gmail.com>
2025-12-06 17:55:02 +0530

    frontend created

---
+++ src/components/login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <input type="text" className="form-control" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default login
