01cdb7dfbd249b08b84bc2bae28df4940be3ee13
alfinroy05 <alfinroy2005@gmail.com>
2025-12-08 23:47:14 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Pages/Login';
 import Dashboard from './Pages/Dashboard';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       
-      <Login/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
