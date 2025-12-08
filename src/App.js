890244c5b29a702a8103849578ed76306c35d007
alfinroy05 <alfinroy2005@gmail.com>
2025-12-08 23:55:32 +0530

    profile done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './Pages/Login';
 import Dashboard from './Pages/Dashboard';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/profile' element={<Profile/>}/>
       </Routes>
       </BrowserRouter>
     </div>
